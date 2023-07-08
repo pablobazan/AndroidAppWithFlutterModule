2a84d0ce8f9aad5546734c0c334b2662101b0452
Pablo Bazan <pablo.bazan@mercadolibre.com>
2023-07-07 22:25:29 -0300

    added start activity Flutter

--- app/src/main/java/com/example/androidappwithfluttermodule/MainActivity.kt
+++ app/src/main/java/com/example/androidappwithfluttermodule/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.example.androidappwithfluttermodule
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.core.content.ContextCompat.startActivity
 import com.example.androidappwithfluttermodule.ui.theme.AndroidAppWithFlutterModuleTheme
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +27,7 @@ class MainActivity : ComponentActivity() {
             AndroidAppWithFlutterModuleTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    StartFlutterModule(onClick = { openFlutterActivity(this) })
                 }
             }
         }
@@ -27,17 +35,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun StartFlutterModule(onClick: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0xFF0175C2)),
+    ) {
+        Button(
+            onClick = onClick,
+            modifier = Modifier.align(Alignment.Center),
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Black),
+        ) {
+            Text(text = "Open Flutter Module", color = Color.White)
+        }
+    }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidAppWithFlutterModuleTheme {
-        Greeting("Android")
-    }
+private fun openFlutterActivity(context: Context) {
+    val intent = FlutterActivity.createDefaultIntent(context)
+    startActivity(context, intent, null)
 }
